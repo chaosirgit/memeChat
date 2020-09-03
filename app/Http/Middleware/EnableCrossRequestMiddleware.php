534ba64ca3f60514739c94c0d82309c31487e4ea
chaosir <qq278935403@gmail.com>
2020-09-03 16:12:14 +0800

    chaosir

--- app/Http/Middleware/EnableCrossRequestMiddleware.php
+++ app/Http/Middleware/EnableCrossRequestMiddleware.php
@@ -16,14 +16,14 @@ class EnableCrossRequestMiddleware
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $origin   = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : 'http://localhost:8081';
+        $origin   = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '*';
 //        $response->header('Access-Control-Allow-Origin', $origin);
 //        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
 //        $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
 //        $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS, DELETE');
 //        $response->header('Access-Control-Allow-Credentials', 'true');
         $response->headers->add([
-//            'Access-Control-Allow-Origin'=> $origin,
+            'Access-Control-Allow-Origin'=> $origin,
             'Access-Control-Allow-Methods'=>'GET, POST, PATCH, PUT, OPTIONS, DELETE',
             'Access-Control-Allow-Headers'=>'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN',
             'Access-Control-Expose-Headers' => 'Authorization, authenticated',
